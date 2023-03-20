8b76f8d400af6b86c488e8ca6977ae67f61057ab
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-20 19:15:30 +0300

    3.1.1.2. Создание и запуск нити [#1016] - mistakes corrected.

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -3,18 +3,6 @@ package ru.job4j.concurrent;
 public class ConcurrentOutput {
 
     public static void main(String[] args) {
-
-        /*
-        Создание Thread явно, без использования лямбды:
-        Thread another = new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println(Thread.currentThread().getName());
-                    }
-                }
-        );
-         */
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
@@ -23,11 +11,6 @@ public class ConcurrentOutput {
         );
         another.start();
         second.start();
-        /*
-        Данный метод, в отличие от start() не заставляет выполнять операции в отдельной нити, а вызывает указанное в конструкторе напрямую.
-        another.run();
-         */
         System.out.println(Thread.currentThread().getName());
     }
-
 }
