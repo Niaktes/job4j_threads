ffa5ea5d89002199d863706428167d7f400f59fc
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-20 18:56:51 +0300

    3.1.1.4. Режим ожидания. [#231217] - Loading simulator developed.

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 100; i++) {
+                            System.out.print("\rLoading: " + i + "%");
+                            Thread.sleep(100);
+                        }
+                        System.out.println("\nLoading complete.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+
+}
