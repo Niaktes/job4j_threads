cc732f62ec0ab6d9cd77599f3fd29d85dccc341d
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-20 19:27:53 +0300

    3.1.1.4. Режим ожидания. [#231217] - Bug fixed.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,9 +8,9 @@ public class Wget {
                     try {
                         for (int i = 0; i <= 100; i++) {
                             System.out.print("\rLoading: " + i + "%");
-                            Thread.sleep(100);
+                            Thread.sleep(1000);
                         }
-                        System.out.println("\nLoading complete.");
+                        System.out.println(System.lineSeparator() + "Loading complete!");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
