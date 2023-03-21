936b160c8c12721f43a5425100a90037d41c5521
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-21 21:49:28 +0300

    3.1.1.11. Отладка в многопоточном приложении [#504954 #403051] - Concurrent debug example.

---
+++ src/main/java/ru/job4j/concurrent/DebugConcurrentDemo.java
@@ -0,0 +1,40 @@
+package ru.job4j.concurrent;
+
+public class DebugConcurrentDemo {
+
+    public static void main(String[] args) {
+        String name1 = "Первый поток";
+        String name2 = "Второй поток";
+        Thread t1 = new Thread(() -> {
+           try {
+               for (int i = 3; i > 0; i--) {
+                   System.out.println(Thread.currentThread().getName() + " : " + i);
+                   Thread.sleep(10);
+               }
+           } catch (InterruptedException e) {
+               System.out.println(Thread.currentThread().getName() + " прерван ");
+           }
+           System.out.println(Thread.currentThread().getName() + " завершен.");
+        }, name1);
+        Thread t2 = new Thread(() -> {
+           try {
+               for (int i = 10; i < 13; i++) {
+                   System.out.println(Thread.currentThread().getName() + " : " + i);
+                   Thread.sleep(10);
+               }
+           } catch (InterruptedException e) {
+               System.out.println(Thread.currentThread().getName() + " прерван ");
+           }
+           System.out.println(Thread.currentThread().getName() + " завершен.");
+        }, name2);
+        t1.start();
+        t2.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван");
+        }
+        System.out.println("Главный поток завершен.");
+    }
+
+}
