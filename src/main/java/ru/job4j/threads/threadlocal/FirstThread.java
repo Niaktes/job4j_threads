007e91b787f69ccb8ed2c61b39127cea2ef69845
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-21 20:59:11 +0300

    3.1.1.9. ThreadLocal [#504912] - ThreadLocal lesson demo.

---
+++ src/main/java/ru/job4j/threads/threadlocal/FirstThread.java
@@ -0,0 +1,11 @@
+package ru.job4j.threads.threadlocal;
+
+public class FirstThread extends Thread {
+
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 1.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+
+}
