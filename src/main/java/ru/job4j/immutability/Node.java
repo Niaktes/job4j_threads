69d8532d91694b4b60da763aa1f9aaf5ac037830
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-22 15:29:22 +0300

    3.1.2.3. Immutable объекты [#267918] - Node class made immutable.

--- src/main/java/ru/job4j/immutability/Node.java
+++ src/main/java/ru/job4j/immutability/Node.java
@@ -1,24 +1,21 @@
 package ru.job4j.immutability;
 
-public class Node<T> {
+public final class Node<T> {
 
-    private Node<T> next;
-    private T value;
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
 
     public Node<T> getNext() {
         return  next;
     }
 
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 
 }
