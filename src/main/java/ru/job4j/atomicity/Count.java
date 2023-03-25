951b3d4095a6c7895863969a07911922aa60afb4
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-03-25 12:29:48 +0300

    3.1.3.2. JCIP. Настройка библиотеки [#268575] - jcip library work example added.

--- src/main/java/ru/job4j/atomicity/Count.java
+++ src/main/java/ru/job4j/atomicity/Count.java
@@ -1,11 +1,16 @@
 package ru.job4j.atomicity;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
 
+    @GuardedBy("this")
     private int value;
 
     public synchronized void increment() {
-        value++;
+        this.value++;
     }
 
     public synchronized int get() {
