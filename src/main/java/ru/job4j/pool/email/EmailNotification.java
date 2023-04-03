7f552a719ce8fb5f57ac57269b0c0d047c564137
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-04-03 16:55:17 +0300

    3.1.6.2. ExecutorService рассылка почты. [#63097] - EmailNotification class developed.

---
+++ src/main/java/ru/job4j/pool/email/EmailNotification.java
@@ -0,0 +1,34 @@
+package ru.job4j.pool.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        String name = user.getUsername();
+        String email = user.getEmail();
+        String subject = String.format("Notification %s to email %s.", name, email);
+        String body = String.format("Add a new event to %s.", name);
+        pool.submit(() -> send(subject, body, email));
+    }
+
+    public void close() {
+        pool.shutdown();
+        while(!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+}
---
+++ src/main/java/ru/job4j/pool/email/User.java
@@ -0,0 +1,43 @@
+package ru.job4j.pool.email;
+
+public class User {
+
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        if (!username.equals(user.username)) {
+            return false;
+        }
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+}
