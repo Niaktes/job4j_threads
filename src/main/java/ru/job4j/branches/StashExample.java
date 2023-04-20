ae5db66bbc5c7e8d27f375c07544394007a927c1
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-04-20 04:20:49 +0300

    3.2.0.3.4. Временное хранение. git stash. [#504832] - code for stash example added.

---
+++ src/main/java/ru/job4j/branches/StashExample.java
@@ -0,0 +1,10 @@
+package ru.job4j.branches;
+
+public class StashExample {
+
+    public static void main(String[] args) {
+        System.out.println("Some code.");
+        System.out.println("Some more code.");
+    }
+
+}
